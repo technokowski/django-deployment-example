80e8697a4c7ea251a04e1b4a5314fbb9bad0fdf4
technokowski <gabrielkavaloski@gmail.com>
2019-06-23 17:19:37 -0700

    first

---
+++ learning_users/basic_app/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from basic_app.models import UserProfileInfoForm
+
+# Register your models here.
+
+
+admin.site.register(UserProfileInfoForm)
